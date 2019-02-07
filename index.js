3f2a685d954a902153901dcd217cd25bad2c45a4
carlos <cgarciapieto@gmail.com>
2019-02-07 16:20:23 -0600

    comments

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 $(".red").addClass("changered");
-
+//uses jquery to call the class and change the background of the first div to a background for green
 $("#myElement>div:first").addClass("changegreen");
 // $("#myElement:div").addClass("changeblue");
 $("#myElement:nth-child(3)").addClass("changepink");
