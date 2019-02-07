05923d1443c4e8603efced592c3b470e4f1db1bb
carlos <cgarciapieto@gmail.com>
2019-02-07 16:18:30 -0600

    afternnon

---
+++ index.js
@@ -0,0 +1,5 @@
+$(".red").addClass("changered");
+
+$("#myElement>div:first").addClass("changegreen");
+// $("#myElement:div").addClass("changeblue");
+$("#myElement:nth-child(3)").addClass("changepink");
